d9ffa5847d0a20d95ef6a4e5113d208e020699b8
saadkhan6969 <ayazsaad57@gmail.com>
2022-10-30 15:10:51 +0500

    Work Complete

---
+++ jj1/app.js
@@ -0,0 +1,45 @@
+alert("Is That A")
+window.alert("Supraaaaaaaaa")
+
+var a =20
+var b=10
+var c=a+b
+alert(c)
+
+
+var abc="Hemllo"
+alert(abc)
+
+var a =20
+var b=10
+var c=a-b
+alert(c)
+
+var a =20
+var b=10
+var c=a*b
+alert(c)
+
+
+var a =20
+var b =10
+var c =a/b
+alert(c)
+
+
+var a =20
+var b ="10"
+var c =a+b
+alert(c)
+
+var a=10
+var b =20
+a++
+b--
+console.log(a)
+console.log(b)
+
+var a=20
+var b=30
+var c=++a - --b
+console.log(c)
